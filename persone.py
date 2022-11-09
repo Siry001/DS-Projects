828389befab9a254779c5fe854fed995d150b5c2
Mohamed Samy (S I R Y) <115507863+Siry001@users.noreply.github.com>
2022-11-09 21:08:05 +0200

    Delete persone.py

--- persone.py
+++
@@ -1,9 +0,0 @@
-class persone:
-    def __init__(self,name,age,tall,color,region):
-        self.name=name
-        self.age=age
-        self.tall=tall
-        self.color=color
-        self.region=region
-persone1=persone('siry','19','176','white','egypt')
-print(persone1.name , persone1.age , persone1.tall , persone1.color , persone1.region )
